b39ca26e032bb932e28785ca1f2bf8812cda007a
Fatima <fatimachaudhry0777@gmail.com>
2026-09-24 12:36:18 -0700

    fix email smtp

--- backend/src/config/sendEmails.js
+++ backend/src/config/sendEmails.js
@@ -1,10 +1,11 @@
-import nodemailer from "nodemailer"
-
+import nodemailer from "nodemailer";
 
 export const sendEmails = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        pass: process.env.EMAIL_PASS,
-        user: process.env.EMAIL_USER
-    }
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
